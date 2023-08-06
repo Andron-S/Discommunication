1f186283e9cae472f65470d83bda8af2fa43e188
TorgunI <torgunivan@gmail.com>
2023-08-06 16:34:09 +0300

    EatAbility.cs

--- Assets/Scripts/Abilities/EatAbility.cs
+++ Assets/Scripts/Abilities/EatAbility.cs
@@ -14,8 +14,8 @@ public class EatAbility : MonoBehaviour
 
     private float _delayCounter;
     private bool _isActivateRecharged;
-    private Ray _DebugRayAbility;
-
+    private Vector2 _currentPlayerDirection;
+    private LayerMask _ignoredLayer;
 
     public float Damage { get => _damage; set => _damage = value; }
 
@@ -28,12 +28,16 @@ public class EatAbility : MonoBehaviour
         _range = 2f;
         _activateDelay = 4;
         _isActivateRecharged = true;
+
+        PlayerMovement.OnRotated += GetPlayerDirection;
+
+        _ignoredLayer = gameObject.transform.parent.gameObject.layer;
     }
 
     private void Update()
     {
-        _DebugRayAbility = new Ray(_activatePoint.position, _activatePoint.right);
-        Debug.DrawRay(_activatePoint.position, transform.right * _range, Color.red);
+        Debug.DrawRay(_activatePoint.position, _currentPlayerDirection * _range);
+        Debug.Log(_currentPlayerDirection);
     }
 
     public int TryEat()
@@ -43,11 +47,11 @@ public class EatAbility : MonoBehaviour
             return 0;
         }
 
-        RaycastHit2D[] hittedArray = Physics2D.RaycastAll(_activatePoint.position, _activatePoint.right);
+        RaycastHit2D[] hittedArray = Physics2D.RaycastAll(_activatePoint.position, _currentPlayerDirection * _range);
 
         foreach (var hitted in hittedArray)
         {
-            if (hitted.collider.TryGetComponent(out Player player) == false)
+            if(hitted.collider.gameObject.layer != _ignoredLayer)
             {
                 if (hitted.collider.TryGetComponent(out IDamageable damageable))
                 {
@@ -56,10 +60,11 @@ public class EatAbility : MonoBehaviour
 
                     var attackDelayCorutine = StartCoroutine(CalculatingAttackDelay());
 
-                    return (int)Damage; // how get 
+                    return (int)Damage;
                 }
             }
         }
+
         return 0;
     }
 
@@ -69,6 +74,11 @@ public class EatAbility : MonoBehaviour
         yield return new WaitForSeconds(_activateDelay);
 
         _isActivateRecharged = true;
+    }
+
+    public void GetPlayerDirection(Vector2 playerDirection)
+    {
+        _currentPlayerDirection = playerDirection;
     }
 
     private void RaiseDelayAbilityCounter()
