7ef0336985c0864754950e8b5b94305a9e720637
PavelOotsutsuki <109985069+PavelOotsutsuki@users.noreply.github.com>
2023-08-06 15:24:11 +0300

    Weapon changed

--- Assets/Scripts/Weapons/Bullet.cs
+++ Assets/Scripts/Weapons/Bullet.cs
@@ -9,39 +9,30 @@ public class Bullet : MonoBehaviour
     [SerializeField] private float _speed = 50;
 
     private float _flyTime = 0;
-
-    private void Update()
-    {
-        Debug.Log(transform.position);
-    }
+    private float _maxFlyTime;
 
     public void FlyLive()
     {
+        _maxFlyTime = 10;
+        StartCoroutine(Fly());
+    }
+
+    public void MeleeLive()
+    {
+        _speed = 0.3f;
+        _maxFlyTime = 0.3f;
         StartCoroutine(Fly());
     }
 
     private IEnumerator Fly()
     {
-        while (Physics2D.RaycastAll(transform.position, transform.up).Length == 0 && _flyTime < 10)
+        while (_flyTime < _maxFlyTime)
         {
             transform.position += transform.up * _speed * Time.deltaTime * -1;
             _flyTime += Time.deltaTime;
             yield return null;
         }
 
-        if (Physics2D.RaycastAll(transform.position, transform.up).Length > 0)
-        {
-            if (Physics2D.RaycastAll(transform.position, transform.up)[0].collider.TryGetComponent<IDamageable>(out IDamageable liver))
-            {
-                Attack(liver);
-            }
-        }
-
         Destroy(gameObject);
-    }
-
-    private void Attack(IDamageable liver)
-    {
-        liver.TakeDamage(_damage);
     }
 }
